374b53dba95d7c1ee22e145b52473dc8f36abe43
Jasper <jaspershemilt@virginmedia.co.uk>
2015-03-19 18:39:07 +0000

    getRGBBins

--- matLab/ColorSpaceStats/Run_1_RGB_Bin_Allocation.m
+++ matLab/ColorSpaceStats/Run_1_RGB_Bin_Allocation.m
@@ -1,11 +1,2 @@
-function [ rgbBin] = getRGBBins(dirName,name)
-% dirName is the path to a directory of 24bit RGB jpg images
-% name is the name of the variable to output
-dataMin  = 0; dataMax = 255; 
-rgbBin = Bin([dataMax+1,dataMax+1,dataMax+1],[dataMin,dataMin,dataMin],[dataMax,dataMax,dataMax]);
-rgbBin.name = name;
-rgbBin.axisNames = ['R','G','B'];
-rgbBin = rgbBin.addDirectory(dirName);
-eval([name,' = ',rgbBin]);
-save(strcat(dirName,'/',name),name);
-end
+
+getRGBBins('/Users/jaspershemilt/Developer/projects/Color-Space-Stats/matLab/ColorSpaceStats/Skin/IndividualSkin/JSkin/Index/Pad/Small','RGB_JSkin_Index_Pad_Bin')
