9fe773879d361b62fab79cce4ead442252cdb80e
Jasper <jaspershemilt@virginmedia.co.uk>
2015-03-24 22:21:29 +0000

    RunScripts

---
+++ matLab/ColorSpaceStats/runAll.m
@@ -0,0 +1,3 @@
+dirName='/Users/jaspershemilt/Developer/projects/Color-Space-Stats/matLab/ColorSpaceStats/Skin/IndividualSkin/JSkin/Index/Pad/Small';
+name='RGB_JSkin_Index_Pad_Bin';
+run_all( dirName,name)
