0430d3e45f8b36c8f88df1674e65003be100d587
Jasper <jaspershemilt@virginmedia.co.uk>
2015-04-16 22:16:52 +0100

    bin vis stuff

--- matLab/ColorSpaceStats/runCombineAll.m
+++ matLab/ColorSpaceStats/runCombineAll.m
@@ -32,56 +32,73 @@ partNames = {'Hand_Bin', ...
 
 
 for i = 1:3
-    indDirName = indDirNames{i};
-    indName = indNames{i};
-    outDirName = strcat(rootDirName,indDirName,'/comb');
+    % Setup Output Directory
+    outDirName = strcat(rootDirName,indDirNames{i},'comb/');
     mkdir(outDirName);
-    mkdir(strcat(outDirName,'/mathematica'));
+    mkdir(strcat(outDirName,'mathematica/'));
     for s = 1:8
-        stageName = stageNames{s};
-        partDirName = partDirNames{1};
-        name = strcat(indName,partNames{1},stageName);
-        sprintf(strcat('%d : Combining : ',name), s)
-        load(strcat(rootDirName,indDirName,partDirName,name,'.mat'))
-        out = eval(name);
-        outName = strcat(indName,'Bin',stageName);
-        out.name = outName;
+        % Load The first bin
+        name = strcat(indNames{i},partNames{1},stageNames{s});
+        sprintf(strcat('%d : Loading : ',name), s)
+        load(strcat(rootDirName, indDirNames{i}, partDirNames{1}, name,'.mat'))
+        % Patch
         if s <=2
-            out.axisNames = ['R','G','B'];
+            eval([name,'.axisNames',' = [''R'',''G'',''B'']'])
+            % out.axisNames = ['R','G','B'];
+        elseif s <= 4
+            eval([name,'.axisNames',' = [''L'',''Ca'',''Cb'']']);
         else
-            out.axisNames = ['L','Ca','Cb'];
+            eval([name,'.axisNames',' = [''Ca'',''Cb'']']);
         end
+        eval([name,' = ',name,'.norm']);
+        save(strcat(rootDirName, indDirNames{i}, partDirNames{1}, name),name);
+        % Equate out with first bin
+        out = eval(name);
+        outName = strcat(indNames{i},'Bin',stageNames{s});
+        out.name = outName;
         for p = 2:5
-            partDirName = partDirNames{p};
-            partName = partNames{p};
-            load(strcat(rootDirName,indDirName, partDirName, indName, partName, stageName,'.mat'))
-            sprintf(strcat('%d : ',indName, partName, stageName), p)
-            sprintf(strcat('%d : loading : ',rootDirName,indDirName, partDirName, indName, partName, stageName,'.mat'), p)
+            % Load The p th bin
+            name = strcat(indNames{i},partNames{p},stageNames{s});
+            sprintf(strcat('%d : Loading : ',name), s)
+            load(strcat(rootDirName, indDirNames{i}, partDirNames{p}, name,'.mat'));
+            % Patch
+            if s <=2
+                eval([name,'.axisNames',' = [''R'',''G'',''B'']'])
+                % out.axisNames = ['R','G','B'];
+            elseif s <= 4
+                eval([name,'.axisNames',' = [''L'',''Ca'',''Cb'']']);
+            else
+                eval([name,'.axisNames',' = [''Ca'',''Cb'']']);
+            end
+            eval([name,' = ',name,'.norm']);
+            save(strcat(rootDirName, indDirNames{i}, partDirNames{p}, name),name);
+            % add bins to out
+            sprintf(strcat('%d : loading : ',rootDirName,indDirNames{i}, partDirNames{p}, name,'.mat'), p)
             out = out.add(eval(name));
         end
         
         eval([outName,' = out.norm']);
-        save(strcat(outDirName,'/',outName),outName);
+        save(strcat(outDirName,outName),outName);
         eval(strcat( 'Bin.saveFields(',outName,',''',outDirName,'/mathematica/',outName,''' )'));
     end
 end
 
 
-outDirName = strcat(rootDirName,'/comb');
+outDirName = strcat(rootDirName,'comb');
 mkdir(outDirName);
 mkdir(strcat(outDirName,'/mathematica'));
 for s = 1:8
-        name = strcat(indNames{1},'Bin',stageNames{s});
-        sprintf(strcat('%d : Combining : ',name), s)
-        load(strcat(rootDirName,indDirNames{1},'comb/',name,'.mat'))
-        out = eval(name);
-        outName = strcat('Bin',stageNames{s});
-        out.name = outName;
-        if s <=2
-            out.axisNames = ['R','G','B'];
-        else
-            out.axisNames = ['L','Ca','Cb'];
-        end
+    name = strcat(indNames{1},'Bin',stageNames{s});
+    sprintf(strcat('%d : Combining : ',name), s)
+    load(strcat(rootDirName,indDirNames{1},'comb/',name,'.mat'))
+    out = eval(name);
+    outName = strcat('Bin',stageNames{s});
+    out.name = outName;
+    if s <=2
+        out.axisNames = ['R','G','B'];
+    else
+        out.axisNames = ['L','Ca','Cb'];
+    end
     
     for i = 2:3
         name = strcat(indNames{i},'Bin',stageNames{s});
